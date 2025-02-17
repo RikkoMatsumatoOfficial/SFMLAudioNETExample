d00abd5805ad8c03a9755ef11532c3bec801b79a
Rikko Matsumato <kuttimsx@hotmail.com>
2025-02-17 14:29:59 +0200

    Add files via upload

---
+++ SFMLAudioNETExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("SFMLAudioNETExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFMLAudioNETExample")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d55de560-4751-4733-adb6-87a4296b6b59")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
